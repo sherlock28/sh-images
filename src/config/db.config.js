c90d9b5db8c580181d9649ed1e0ffe2602a6392f
Cáceres Rodolfo <caceresrodolfo28@gmail.com>
2021-07-10 03:23:56 -0300

    The database configuration is added

---
+++ src/config/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 3306,
+  database: process.env.DB_DATABASE || "testsh",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+};
