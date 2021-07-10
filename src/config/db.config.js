c3cda8e6a8dbf225c666756411a62049f4ad3897
Cáceres Rodolfo <caceresrodolfo28@gmail.com>
2021-07-10 07:08:12 -0300

    Database configuration is modify

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,8 +1,22 @@
-module.exports = {
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 3306,
-  database: process.env.DB_DATABASE || "testsh",
-  dialect: process.env.DB_DIALECT || "mysql",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
+const db_development = {
+  host: process.env.DB_DEV_HOST || "localhost",
+  port: process.env.DB_DEV_PORT || 3306,
+  database: process.env.DB_DEV_DATABASE || "testsh",
+  dialect: process.env.DB_DEV_DIALECT || "mysql",
+  user: process.env.DB_DEV_USER || "root",
+  password: process.env.DB_DEV_PASSWORD || "",
 };
+
+const db_production = {
+  host: process.env.DB_PRO_HOST || "localhost",
+  port: process.env.DB_PRO_PORT || 3306,
+  database: process.env.DB_PRO_DATABASE || "testsh",
+  dialect: process.env.DB_PRO_DIALECT || "mysql",
+  user: process.env.DB_PRO_USER || "root",
+  password: process.env.DB_PRO_PASSWORD || "",
+};
+
+const db_config =
+  process.env.NODE_ENV === "production" ? db_production : db_development;
+
+module.exports = db_config;
