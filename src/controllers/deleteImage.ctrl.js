f1cfd673cebf5703b5edb6826dde1312f5f42f80
Cáceres Rodolfo <caceresrodolfo28@gmail.com>
2021-07-10 06:42:12 -0300

    Endpoint is added to delete images

--- src/controllers/deleteImage.ctrl.js
+++ src/controllers/deleteImage.ctrl.js
@@ -1,15 +1,16 @@
 const { cloudinary } = require("../config");
-const connection = require("../database/dbconnection")();
+const connection = require("../database/dbconnection");
 
 const deleteImage = async (req, res) => {
   try {
-    const { id_image } = req.params;
+    const { public_id } = req.params;
 
-    // const product = await Product.findByIdAndRemove(id_product);
+    const query = `DELETE FROM images WHERE public_id='${public_id}'`;
+    await connection.query(query);
 
-    // const result = await cloudinary.uploader.destroy(product.public_id);
+    await cloudinary.uploader.destroy(public_id);
 
-    res.json({ status: "Ok", message: "Product successfully deleted" });
+    res.json({ status: "Ok", message: "Image deleted successfully" });
   } catch (err) {
     console.error(err);
     res.status(500).json({ status: "Error", message: "Internal server error" });
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const { postImages, deleteImage } = require("../controllers");
 
 router.post("/image", postImages);
-router.delete("/image/:id", deleteImage);
+router.delete("/image/:public_id", deleteImage);
 
 module.exports = router;
