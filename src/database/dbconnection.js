e8074a8a22d47dffe8b2df837c6268bfb903ab6b
Cáceres Rodolfo <caceresrodolfo28@gmail.com>
2021-07-10 06:25:51 -0300

    Database connection is modify

--- src/database/dbconnection.js
+++ src/database/dbconnection.js
@@ -1,11 +1,20 @@
-const mysql = require("mysql2");
+const mysql = require("mysql");
 const { db_config } = require("../config");
 
-module.exports = () => {
-  return mysql.createConnection({
-    host: db_config.host,
-    user: db_config.user,
-    password: db_config.password,
-    database: db_config.database,
-  });
-};
+const mysqlConnection = mysql.createConnection({
+  host: db_config.host,
+  user: db_config.user,
+  password: db_config.password,
+  database: db_config.database,
+});
+
+mysqlConnection.connect(err => {
+  if (err) {
+    console.log(err);
+    return;
+  } else {
+    console.log("MySQL connected");
+  }
+});
+
+module.exports = mysqlConnection;
