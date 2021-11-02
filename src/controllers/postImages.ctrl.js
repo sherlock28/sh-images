b8c64c78ab01c6f670a1645d1ee42db03cb88015
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2021-11-01 21:57:23 -0300

    Image repository used

--- src/controllers/postImages.ctrl.js
+++ src/controllers/postImages.ctrl.js
@@ -1,19 +1,20 @@
 const { cloudinary } = require("../config");
 const fs = require("fs-extra");
 const { connect, disconnect } = require("../database/dbconnection");
+const imagestRepository = require("../repository/images.repo");
 
 const postImages = async (req, res) => {
   const connection = await connect();
   const { idHouse } = req.body;
-  if(!idHouse) {
+  if (!idHouse) {
     disconnect(connection);
     res.status(400).json({ status: "Error", message: "idHouse required" });
   }
 
   try {
-    const images = req.files.map(file => file.path);
+    const images = req.files.map((file) => file.path);
     console.log("uploading images...");
-    const promisesSave = images.map(image => {
+    const promisesSave = images.map((image) => {
       return cloudinary.uploader.upload(image, {
         resource_type: "image",
         folder: process.env.CLOUDINARY_FOLDER_IMAGES,
@@ -23,28 +24,34 @@ const postImages = async (req, res) => {
     const response = await Promise.all(promisesSave);
     console.log("images uploaded");
 
-    const promisesRemove = images.map(image => fs.unlink(image));
+    const promisesRemove = images.map((image) => fs.unlink(image));
     await Promise.all(promisesRemove);
 
-    const imagesSaved = response.map(res => {
+    const imagesSaved = response.map((res) => {
       return { imageURL: res.secure_url, publib_id: res.public_id };
     });
     console.log("saving to database...");
-    const promisesStore = imagesSaved.map(image => {
-      const query = `INSERT INTO ownerships_images (imageurl, public_id, inmueble_id) VALUES ('${image.imageURL}','${image.publib_id}','${idHouse}')`;
-      return connection.query(query);
-    });
-    await Promise.all(promisesStore);
+    imagestRepository
+      .saveImagesURL(imagesSaved, idHouse)
+      .then(() => {
+        console.log("images saved");
+        disconnect(connection);
+        res.status(201).json({
+          status: "OK",
+          message: "Images saved successfully",
+          data: {
+            images: "imagesSaved",
+          },
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        disconnect(connection);
+        res
+          .status(503)
+          .json({ status: "Error", message: "Error saving images" });
+      });
 
-    disconnect(connection);
-
-    res.status(201).json({
-      status: "Ok",
-      message: "Images saved successfully",
-      data: {
-        images: "imagesSaved",
-      },
-    });
   } catch (err) {
     console.log(err);
     disconnect(connection);
