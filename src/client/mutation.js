4df74ffc3aa9eeefdd9f64ea7e18962f42a23231
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-10-03 01:36:58 -0300

    Added graphql mutation for delete an image

--- src/client/mutation.js
+++ src/client/mutation.js
@@ -2,7 +2,7 @@ const { gql } = require("graphql-request");
 
 const AddImageMutation = gql`
     mutation AddImage($imageurl: String!, $ownerships_id: bigint!, $public_id: String!) {
-    insert_sh_ownerships_images_one(object: { imageurl: $imageurl, ownerships_id: $ownerships_id, public_id: $public_id }) {
+        insert_sh_ownerships_images_one(object: { imageurl: $imageurl, ownerships_id: $ownerships_id, public_id: $public_id }) {
             imageurl
             ownerships_id
             public_id
@@ -10,4 +10,12 @@ const AddImageMutation = gql`
     }    
 `;
 
-module.exports = { AddImageMutation }; 
+const DeleteImageMutation = gql`
+    mutation DeleteImage($public_id: String!) {
+        delete_sh_ownerships_images(where: {public_id: {_eq: $public_id}}) {
+            affected_rows
+        }
+    }
+`;
+
+module.exports = { AddImageMutation, DeleteImageMutation }; 
