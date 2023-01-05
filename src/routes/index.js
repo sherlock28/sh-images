4326e34efcf5f418edd37e1b7b4517f71d0632e7
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2023-01-05 01:36:41 -0300

    Added: New route '/' for app information

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,5 +3,10 @@ const { postImages, deleteImage } = require("../controllers");
 
 router.post("/images", postImages);
 router.delete("/images", deleteImage);
+router.get("/", (req, res) => res.json({
+    api: "post-images",
+    description: "API to upload images to a cloudinary folder",
+    version: "v1"
+}));
 
 module.exports = router;
